1b2fdb69c4fe636e09b45a8539ba3c144ea6d1dd
Donald Gray <donald.gray@digirati.com>
2020-04-17 14:47:36 +0100

    Add regionalised bucket object

---
+++ DLCS.Model.Tests/Storage/RegionalisedObjectInBucketTests.cs
@@ -0,0 +1,119 @@
+﻿using DLCS.Model.Storage;
+using FluentAssertions;
+using Xunit;
+
+namespace DLCS.Model.Tests.Storage
+{
+    public class RegionalisedObjectInBucketTests
+    {
+        [Fact]
+        public void Parse_Correct_S3Qualified()
+        {
+            // Arrange
+            const string uri = "s3://eu-west-1/dlcs-storage/2/1/foo-bar";
+            var expected = new RegionalisedObjectInBucket
+            {
+                Bucket = "dlcs-storage",
+                Key = "2/1/foo-bar",
+                Region = "eu-west-1"
+            };
+
+            // Act
+            var actual = RegionalisedObjectInBucket.Parse(uri);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Theory]
+        [InlineData("http://s3-eu-west-1.amazonaws.com/dlcs-storage/2/1/foo-bar")]
+        [InlineData("https://s3.eu-west-1.amazonaws.com/dlcs-storage/2/1/foo-bar")]
+        public void Parse_Correct_Http1(string uri)
+        {
+            // Arrange
+            var expected = new RegionalisedObjectInBucket
+            {
+                Bucket = "dlcs-storage",
+                Key = "2/1/foo-bar",
+                Region = "eu-west-1"
+            };
+
+            // Act
+            var actual = RegionalisedObjectInBucket.Parse(uri);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Theory]
+        [InlineData("http://dlcs-storage.s3.amazonaws.com/2/1/foo-bar")]
+        [InlineData("https://dlcs-storage.s3.amazonaws.com/2/1/foo-bar")]
+        public void Parse_Correct_Http2(string uri)
+        {
+            // Arrange
+            var expected = new RegionalisedObjectInBucket
+            {
+                Bucket = "dlcs-storage",
+                Key = "2/1/foo-bar",
+            };
+
+            // Act
+            var actual = RegionalisedObjectInBucket.Parse(uri);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Theory]
+        [InlineData("http://dlcs-storage.s3.eu-west-1.amazonaws.com/2/1/foo-bar")]
+        [InlineData("https://dlcs-storage.s3.eu-west-1.amazonaws.com/2/1/foo-bar")]
+        public void Parse_Correct_Http3(string uri)
+        {
+            // Arrange
+            var expected = new RegionalisedObjectInBucket
+            {
+                Bucket = "dlcs-storage",
+                Key = "2/1/foo-bar",
+                Region = "eu-west-1"
+            };
+
+            // Act
+            var actual = RegionalisedObjectInBucket.Parse(uri);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Theory]
+        [InlineData("http://s3.amazonaws.com/dlcs-storage/2/1/foo-bar")]
+        [InlineData("https://s3.amazonaws.com/dlcs-storage/2/1/foo-bar")]
+        public void Parse_Correct_Http4(string uri)
+        {
+            // Arrange
+            var expected = new RegionalisedObjectInBucket
+            {
+                Bucket = "dlcs-storage",
+                Key = "2/1/foo-bar",
+            };
+
+            // Act
+            var actual = RegionalisedObjectInBucket.Parse(uri);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void Parse_Null_IfNoMatches()
+        {
+            // Arrange
+            const string uri = "http://example.org";
+
+            // Act
+            var actual = RegionalisedObjectInBucket.Parse(uri);
+
+            // Assert
+            actual.Should().BeNull();
+        }
+    }
+}
---
+++ DLCS.Model/Storage/RegionalisedObjectInBucket.cs
@@ -0,0 +1,95 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DLCS.Model.Storage
+{
+    public class RegionalisedObjectInBucket : ObjectInBucket
+    {
+        public string Region { get; set; }
+        
+        // NOTE(DG) Regex's and logic moved from deliverator
+        private static readonly Regex RegexS3Qualified = new Regex(@"s3\:\/\/(.*?)\/(.*?)\/(.*)", RegexOptions.Compiled);
+        
+        // us-east-1 uses period instead of dash
+        private static readonly Regex RegexHttp1 = new Regex(@"^http[s]?\:\/\/s3[\-\.](\S+\-\S+\-\d)\.amazonaws\.com\/(.*?)\/(.*)$", RegexOptions.Compiled); 
+        
+        // no region for this one
+        private static readonly Regex RegexHttp2 = new Regex(@"^http[s]?:\/\/(.*?)\.s3\.amazonaws\.com\/(.*)$", RegexOptions.Compiled); 
+        
+        // includes region
+        private static readonly Regex RegexHttp3 = new Regex(@"^http[s]?:\/\/(.*?)\.s3\.(.*?)\.amazonaws\.com\/(.*)$", RegexOptions.Compiled); 
+        
+        // bucket name in path (assumes same region as caller)
+        private static readonly Regex RegexHttp4 = new Regex(@"^http[s]?:\/\/s3\.amazonaws\.com\/(.*?)\/(.*)$", RegexOptions.Compiled); 
+
+        public static RegionalisedObjectInBucket Parse(string uri)
+        {
+            if (TryParseBucketInfo(RegexS3Qualified, uri, out var result, qualified: true))
+            {
+                return result;
+            }
+
+            if (TryParseBucketInfo(RegexHttp1, uri, out result, qualified: true))
+            {
+                return result;
+            }
+
+            if (TryParseBucketInfo(RegexHttp2, uri, out result, qualified: false))
+            {
+                return result;
+            }
+
+            if (TryParseBucketInfo(RegexHttp3, uri, out result, qualified: true, following: true))
+            {
+                return result;
+            }
+
+            if (TryParseBucketInfo(RegexHttp4, uri, out result, qualified: false))
+            {
+                return result;
+            }
+
+            return null;
+        }
+
+        private static bool TryParseBucketInfo(Regex regex, string s, out RegionalisedObjectInBucket bucket,
+            bool qualified = false, bool following = false)
+        {
+            var match = regex.Match(s);
+
+            if (!match.Success)
+            {
+                bucket = null;
+                return false;
+            }
+
+            if (qualified)
+            {
+                if (following)
+                {
+                    bucket = new RegionalisedObjectInBucket
+                    {
+                        Region = match.Groups[2].Value,
+                        Bucket = match.Groups[1].Value,
+                        Key = match.Groups[3].Value
+                    };
+                    return true;
+                }
+
+                bucket = new RegionalisedObjectInBucket
+                {
+                    Region = match.Groups[1].Value,
+                    Bucket = match.Groups[2].Value,
+                    Key = match.Groups[3].Value
+                };
+                return true;
+            }
+
+            bucket = new RegionalisedObjectInBucket
+            {
+                Bucket = match.Groups[1].Value,
+                Key = match.Groups[2].Value
+            };
+            return true;
+        }
+    }
+}
