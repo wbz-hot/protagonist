6da6f18d3929350038a1f94be907fe2cf1db25d3
Donald Gray <donald.gray@digirati.com>
2021-06-03 09:09:53 +0100

    PoC assuming customer as admin

--- Portal/Pages/Admin/Index.cshtml.cs
+++ Portal/Pages/Admin/Index.cshtml.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Portal.Pages.Admin
 {
-    public class Index : PageModel
+    public class IndexModel : PageModel
     {
+        [BindProperty]
+        public int? AssumeRole { get; set; }
+        
         public void OnGet()
         {
             
+        }
+
+        public async Task OnPostAsync()
+        {
+            // TODO - this should be moved to mediatr handler with claims generation in a shared service
+            var claims = new List<Claim>
+            {
+                new (ClaimTypes.Name, User.GetClaimValue(ClaimTypes.Name)),
+                new (ClaimTypes.NameIdentifier, User.GetClaimValue(ClaimTypes.NameIdentifier)),
+                new (ClaimsPrincipalUtils.Claims.Customer, AssumeRole.ToString()),
+                new (ClaimsPrincipalUtils.Claims.Customer, User.GetCustomerId().ToString()),
+                new (ClaimTypes.Role, ClaimsPrincipalUtils.Roles.Customer),
+                new (ClaimTypes.Role, ClaimsPrincipalUtils.Roles.Admin),
+                new (ClaimTypes.Role, "Assuming"),
+            };
+
+            var apiCreds = User.GetApiCredentials();
+            if (!string.IsNullOrEmpty(apiCreds))
+            {
+                claims.Add(new Claim(ClaimsPrincipalUtils.Claims.ApiCredentials, apiCreds));
+            }
+
+            var newClaimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(newClaimsIdentity), 
+                new AuthenticationProperties());
         }
     }
 }
